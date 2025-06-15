ba657f18d039aca5032343c1e13942dfec0b9f77
Khai Nguyen <tuankhai2k@gmail.com>
2025-06-15 17:28:44 +0700

    add off mode carbon intensity query mode

---
+++ src/sinfonia/carbon_fetcher.py
@@ -0,0 +1,40 @@
+from enum import Enum
+from typing import Tuple, Optional
+
+import requests
+
+
+class CarbonIntensityQueryMode(Enum):
+    REALTIME = 'REALTIME'
+    REPLAY = 'REPLAY'
+    OFF = 'OFF'
+
+
+class RealTimeCarbonIntensityFetcher:
+    ELECTRICITY_MAP_CARBON_INTENSITY_URL = "https://api.electricitymap.org/v3/carbon-intensity/latest"
+    
+    def __init__(
+        self, 
+        auth_token: str,
+        coordinate: Optional[Tuple[float, float]] = None,
+    ):
+        self._params = dict()
+        if coordinate is not None:
+            self._params = {"lat": coordinate[0], "lon": coordinate[1]}
+
+        self._headers = {
+            "auth-token": auth_token
+        }
+    
+    def fetch(self) -> float:
+        """Returns latest carbon intensity (gCO2/kWh) from Electricity Maps"""
+        resp = requests.get(
+            self.ELECTRICITY_MAP_CARBON_INTENSITY_URL,
+            headers=self._headers,
+            params=self._params,
+        )
+    
+        resp.raise_for_status()
+
+        resp_body = resp.json()
+        return resp_body['carbonIntensity']
