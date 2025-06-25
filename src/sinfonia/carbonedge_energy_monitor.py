c2ded0eb0fe70d4ac96c2a7f9bcc7ce2114fa53c
Khai Nguyen <tuankhai2k@gmail.com>
2025-06-25 01:13:57 +0000

    add rapl energy

---
+++ src/sinfonia/carbonedge_energy_monitor.py
@@ -0,0 +1,73 @@
+from typing import Optional
+
+import threading
+import time
+
+import rapl
+
+
+class RAPLEnergyDelta:
+    """Get energy usage since last invocation"""
+    def __init__(self, sample_sec: int):
+        self._s_bef = rapl.RAPLMonitor.sample()
+        self._energy_joules = 0
+
+    def sample(self):
+        s_now = rapl.RAPLMonitor.sample()
+        diff = self._s_bef - s_now
+
+        eu_sum = 0
+        for domain in diff.domains.values():
+            eu_sum += diff.energy(package=domain.name, unit=rapl.JOULES)
+
+        self._energy_joules = eu_sum
+
+    def get_energy_joules(self):
+        return self._energy_joules
+    
+    def get_energy_kwh(self):
+        kJ = self._energy_joules / 1000.0
+        hours = 1 / 3600.0
+        return kJ * hours
+
+
+class RAPLEnergyMonitor:
+    def __init__(self, sample_sec: int):
+        self._sample_sec = sample_sec
+        self._energy_joules = 0
+        self._t: Optional[threading.Thread] = None
+        self._lock = threading.Lock()
+        self._stop_event = threading.Event()
+    
+    def start_monitoring(self):
+        self._t = threading.Thread(target=self._start, daemon=True)
+        self._t.start()
+
+    def stop_monitoring(self):
+        self._stop_event.set()
+        self._t.join()
+
+    def _start(self):
+        while not self._stop_event.is_set():
+            s_bef = rapl.RAPLMonitor.sample()
+            time.sleep(self._sample_sec)
+            s_aft = rapl.RAPLMonitor.sample()
+
+            diff = s_aft - s_bef
+
+            eu_sum = 0
+            for domain in diff.domains.values():
+                eu_sum += diff.energy(package=domain.name, unit=rapl.JOULES)
+
+            with self._lock:
+                self._energy_joules = eu_sum
+
+    def get_energy_joules(self) -> float:
+        with self._lock:
+            return self._energy_joules
+    
+    def get_energy_kwh(self) -> float:
+        with self._lock:
+            kJ = self._energy_joules / 1000.0
+            hours = 1 / 3600.0
+            return kJ * hours
